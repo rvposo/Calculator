87b8fe3b223442a9e493b45ddb6dcccc65cc37c6
Gabriel Raposo <gabrielraposo0@hotmail.com>
2024-07-29 21:34:21 -0300

    Adicionado menu de seleção

--- Program.cs
+++ Program.cs
@@ -4,8 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-       Sum();
+     Menu();
 
+    }
+
+    static void Menu(){
+        Console.Clear();
+
+        Console.WriteLine("1 - Sum");
+        Console.WriteLine("2 - Sub");
+        Console.WriteLine("3 - Div");
+        Console.WriteLine("4 - Multi");
+
+        Console.WriteLine("Choose your option: ");
+        int option = int.Parse(Console.ReadLine());
+
+
+         
+          switch(option){
+             case 1:
+                Sum();
+                break;
+
+             case 2: 
+                Sub();
+                break;
+            
+             case 3:
+                Division();
+                break;
+
+             case 4:
+                Multi();
+                break;
+                  
+          }
     }
     static void Sum()
     {
@@ -17,11 +50,63 @@ class Program
 
         Console.WriteLine("Second value: ");
         float secondValue = float.Parse(Console.ReadLine());
-        
+
         Console.WriteLine("");
 
         float result = firstValue + secondValue;
         Console.WriteLine($"Sum output is: {result}");
+        Console.ReadKey();
+    }
+
+    static void Sub()
+    {
+        Console.Clear();
+
+        Console.WriteLine("First value: ");
+        float firstValue = float.Parse(Console.ReadLine());
+        //float.Parse(Console.ReadLine());
+
+        Console.WriteLine("Second value: ");
+        float secondValue = float.Parse(Console.ReadLine());
+
+        Console.WriteLine("");
+
+        float result = firstValue - secondValue;
+        Console.WriteLine($"Sub output is: {result}");
+        Console.ReadKey();
+    }
+
+    static void Division(){
+        Console.Clear();
+
+
+        Console.WriteLine("First value: ");
+        float firstValue = float.Parse(Console.ReadLine());
+
+        Console.WriteLine("Second value: ");
+        float secondValue = float.Parse(Console.ReadLine());
+
+        float division = firstValue / secondValue;
+
+        Console.WriteLine($"Division result is {division}");
+        Console.ReadKey();
+    }
+
+    static void Multi()
+    {
+        Console.Clear();
+
+        Console.WriteLine("First value: ");
+        float firstValue = float.Parse(Console.ReadLine());
+
+        Console.WriteLine("Second value: ");
+        float secondValue = float.Parse(Console.ReadLine());
+
+        float multi = firstValue * secondValue;
+
+        Console.WriteLine($"Multi result is {multi}");
+
+        Console.ReadKey();
     }
 
 }
